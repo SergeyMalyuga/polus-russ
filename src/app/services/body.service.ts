a752484907f71660ffde922021c7107343b2f308
Sergey Malyuga <sergey@malyuga.ru>
2026-05-22 15:05:00 +0300

    feat(body): создать сервис

---
+++ src/app/services/body.service.ts
@@ -0,0 +1,12 @@
+import { DOCUMENT, inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BodyService {
+  private readonly document = inject(DOCUMENT);
+
+  public setScrollLocked(isNoScroll: boolean) {
+    this.document.body.classList.toggle('no-scroll', isNoScroll);
+  }
+}
