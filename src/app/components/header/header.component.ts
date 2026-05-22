3c26b32fadf85890ee36f452f588a95d0aea187a
Sergey Malyuga <sergey@malyuga.ru>
2026-05-22 15:14:53 +0300

    feat(header): адаптировать меню навигации под мобильные устройства

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,44 @@
 // header.component.ts
-import { Component } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ToggleDirective } from '../../shared/directives/toggle';
+import { NgClass } from '@angular/common';
+import { ViewportService } from '../../services/viewport.service';
+import { FocusOnShowDirective } from '../../shared/directives/focus-on-show.directive';
+import { BodyService } from '../../services/body.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, ToggleDirective, NgClass, FocusOnShowDirective],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private viewportService = inject(ViewportService);
+  private bodyService = inject(BodyService);
+
+  public isNavOpen = signal<boolean>(false);
+
+  constructor() {
+    effect(() => {
+      const isMobile = this.viewportService.isMobile();
+      if (!isMobile) {
+        this.isNavOpen.set(false);
+        this.bodyService.setScrollLocked(false);
+      }
+    });
+  }
+
+  public toggleNavMenu() {
+    this.isNavOpen.set(!this.isNavOpen());
+    this.bodyService.setScrollLocked(this.isNavOpen());
+  }
+
+  public closeNavMenu() {
+    if (this.isNavOpen() && this.viewportService.isMobile()) {
+      this.isNavOpen.set(false);
+      this.bodyService.setScrollLocked(false);
+    }
+  }
+}
