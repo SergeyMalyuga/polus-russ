9fcb6fc6bff2ca82caa589ae2363536032e0126f
Sergey Malyuga <sergey@malyuga.ru>
2026-05-25 20:24:10 +0300

    chore(index): добавить мета тег description

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,9 @@ import { PartnerComponent } from './components/partner/partner.component';
 import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
-  { path: '', component: HeroComponent, title: 'Polus Russ — коммерческие напольные покрытия' },
-  { path: 'products', component: ProductsComponent, title: 'Продукция | Polus Russ' },
-  { path: 'partner', component: PartnerComponent, title: 'Gerflor | Polus Russ' },
-  { path: 'contact', component: ContactComponent, title: 'Контакты | Polus Russ' },
+  { path: '', component: HeroComponent, title: 'Полюс Русс — коммерческие напольные покрытия' },
+  { path: 'products', component: ProductsComponent, title: 'Продукция | Полюс Русс' },
+  { path: 'partner', component: PartnerComponent, title: 'Gerflor | Полюс Русс' },
+  { path: 'contact', component: ContactComponent, title: 'Контакты | Полюс Русс' },
   { path: '**', redirectTo: '' },
 ];
