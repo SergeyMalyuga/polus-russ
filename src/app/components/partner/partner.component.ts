9ce7386abbdf68bb3ee8bb396a125dc00c02da46
Sergey Malyuga <sergey@malyuga.ru>
2026-05-27 13:38:48 +0300

    feat(partner): добавить возможность открытия dialog
    - При клике на кнопку сертификат открывается диалоговое окно

--- src/app/components/partner/partner.component.ts
+++ src/app/components/partner/partner.component.ts
@@ -1,5 +1,7 @@
 // partner.component.ts
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DialogService } from '../../services/dialog.service';
+import { CertificateDialog } from '../certificate-dialog/certificate-dialog';
 
 @Component({
   selector: 'app-partner',
@@ -7,4 +9,7 @@ import { Component } from '@angular/core';
   templateUrl: './partner.component.html',
   styleUrl: './partner.component.scss',
 })
-export class PartnerComponent {}
+export class PartnerComponent {
+  public dialogService = inject(DialogService);
+  protected readonly CertificateDialog = CertificateDialog;
+}
